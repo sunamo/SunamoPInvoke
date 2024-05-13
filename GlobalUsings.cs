6ac40391f084c3098a57d823f58ecfcf80c2e751
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-13 21:32:23 +0200

    Removed all ns to Sunamo*

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -3,10 +3,10 @@ global using System.Text;
 global using System.Diagnostics;
 global using System.Runtime.InteropServices;
 global using System.ComponentModel;
-global using SunamoPInvoke.PInvoke;
-global using SunamoPInvoke.Args;
-global using SunamoPInvoke.Enums;
-global using SunamoPInvoke.Structs;
+
+
+
+
 global using System.Collections.Generic;
 global using System.Collections;
 global using System.IO;
