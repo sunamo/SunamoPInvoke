fb77c682505708b0493452defec853803604c993
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-16 15:35:23 +0200

    buildable, before standardize

--- _sunamo/SunamoExceptions/_AddedToAllCsproj/CASunamoExceptions.cs
+++ _sunamo/SunamoExceptions/_AddedToAllCsproj/CASunamoExceptions.cs
@@ -1,6 +1,6 @@
-namespace SunamoPInvoke._sunamo.SunamoExceptions._AddedToAllCsproj;
+//namespace SunamoPInvoke._sunamo.SunamoExceptions._AddedToAllCsproj;
 
-internal class CASunamoExceptions
+internal class CA
 {
     internal static bool IsListStringWrappedInArray<T>(List<T> v2)
     {
--- _sunamo/SunamoExceptions/_AddedToAllCsproj/FSSunamoExceptions.cs
+++ _sunamo/SunamoExceptions/_AddedToAllCsproj/FSSunamoExceptions.cs
@@ -1,5 +1,7 @@
-namespace SunamoPInvoke._sunamo.SunamoExceptions._AddedToAllCsproj;
-internal class FSSunamoExceptions
+//namespace SunamoPInvoke._sunamo.SunamoExceptions._AddedToAllCsproj;
+
+//namespace SunamoPInvoke._sunamo.SunamoExceptions._AddedToAllCsproj;
+internal class FS
 {
     internal static void CreateUpfoldersPsysicallyUnlessThere(string nad)
     {
